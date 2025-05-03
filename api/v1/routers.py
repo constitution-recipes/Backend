370eec111a86030042d0fdd3ed4d40bb77dbff13
internationalwe <tlsghwns1122@gmail.com>
2025-05-04 00:27:02 +0900

    라우터에 헌법 관련 엔드포인트 추가 및 사용자 스키마에 헌법 관련 필드 추가. 불필요한 아이템 엔드포인트 파일 삭제. 챗 모듈에서 디버깅 로그 추가.

--- api/v1/routers.py
+++ api/v1/routers.py
@@ -1,12 +1,13 @@
 # myapi/api/v1/routers.py
 # C:\Users\agile\PycharmProjects\FastapiProject
 from fastapi import APIRouter
-from api.v1.endpoints import user, recipe, chat, chat_sessions
+from api.v1.endpoints import user, recipe, chat, chat_sessions, constitution
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(chat.router, prefix="/users/chat", tags=["chat"])
 api_router.include_router(recipe.router, prefix="/recipes", tags=["recipes"])
 api_router.include_router(chat_sessions.router, prefix="/chat", tags=["chat"])
+api_router.include_router(constitution.router, prefix="/constitution", tags=["constitution"])
 
 # api_router.include_router(item.router, prefix="/items", tags=["items"])
